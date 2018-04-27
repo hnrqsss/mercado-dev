0e74bf4eeb4427214c77b2de50ad3a03491809a5
hnrqss <hnrq.assuncao@gmail.com>
2018-04-26 21:36:30 -0300

    create sections ad an category in home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 //import './App.css';
 
-import HeaderHome from "./HeaderHome";
+import HeaderHome from './HeaderHome'
+import AdHome from './AdHome'
+import Categories from './Categories'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
     return (
         <div className="App">
             <HeaderHome />
+
+            <div className="container">
+
+                <h3>Últimos anúncios</h3>
+
+                <div className="row">
+                    <AdHome/>
+                    <AdHome/>
+                    <AdHome/>
+                </div>
+
+                <h3>Categorias</h3>
+
+                <div className="row">
+                    <Categories/>
+                </div>
+
+            </div>
+
+            <Footer/>
+
         </div>
     );
   }
