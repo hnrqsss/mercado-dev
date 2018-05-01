e8c11ea5157f281b1151d681d81062863240ceae
hnrqss <hnrq.assuncao@gmail.com>
2018-04-30 22:50:03 -0300

    starts input data on firebase from form submit

--- src/NewAd.js
+++ src/NewAd.js
@@ -1,43 +1,73 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import HeaderAd from './HeaderAd'
+import base, { storage } from './base'
 
 
 class NewAd extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
     
+    handleSubmit(event){
+        const newAd = {
+            nome : this.nome.value,
+            descricao: this.descricao.value,
+            preco: this.preco.value,
+            telefone: this.telefone.value,
+            vendedor: this.vendedor.value,
+            foto: 'http://placehold.it/200x140'
+        }
+        
+        base.push('anuncios',{
+            data: newAd
+        }, (err) => {
+            if(err){
+    
+            }else{
+    
+            }
+        })
+
+        event.preventDefautl()
+    }
 
     render(){
+    
         return(
 
             <div className="container" style={{paddingTop:'120px'}} >
                 <HeaderAd />
                 <h1>Novo Anúncio</h1>
 
-                <form onSubmit={null} >
-                
+                <form onSubmit={this.handleSubmit} >
+
                     <div className="form-group">
                         <label htmlFor='nome'>Nome</label>
-                        <input type='text' className='form-control' id='nome' placeholder='nome' />
+                        <input type='text' className='form-control' id='nome' placeholder='nome' ref={ (ref) => this.nome = ref } />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor='descricao'>Descrição</label>
-                        <input type='text' className='form-control' id='descricao' placeholder='descricao' />
+                        <input type='text' className='form-control' id='descricao' placeholder='descricao' ref={ (ref) => this.descricao = ref } />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor='preco'>Preço</label>
-                        <input type='text' className='form-control' id='preco' placeholder='preco' />
+                        <input type='text' className='form-control' id='preco' placeholder='preco' ref={ (ref) => this.preco = ref } />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor='preco'>Telefone</label>
-                        <input type='text' className='form-control' id='telefone' placeholder='telefone' />
+                        <input type='text' className='form-control' id='telefone' placeholder='telefone' ref={ (ref) => this.telefone = ref } />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor='vendedor'>Vendedor</label>
-                        <input type='text' className='form-control' id='vendedor' placeholder='vendedor' />
+                        <input type='text' className='form-control' id='vendedor' placeholder='vendedor' ref={ (ref) => this.vendedor = ref } />
                     </div>
                     <button type='submit' className='btn btn-primary'>Salvar anúncio</button>
                 </form>
